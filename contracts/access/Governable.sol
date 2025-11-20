fcefdb92a3390b0a9bf56f1b27680430ce8366bd
Gagan U Biradar <138388119+Gaganub@users.noreply.github.com>
2025-11-20 12:51:46 +0530

    Update Governable.sol

--- contracts/access/Governable.sol
+++ contracts/access/Governable.sol
@@ -1,20 +1,28 @@
 // SPDX-License-Identifier: MIT
 
+// Governable.sol - Provides a governance address and related access control modifier.
+// Author: Gaganub
+
 pragma solidity 0.6.12;
 
 contract Governable {
+    /// @notice Address of the current governance owner
     address public gov;
 
+    // ========= Constructor =========
     constructor() public {
         gov = msg.sender;
     }
 
-    modifier onlyGov() {
-        require(msg.sender == gov, "Governable: forbidden");
-        _;
-    }
+   
 
+    // ========= Governance Functions =========
+
+    /// @notice Sets a new governance address
+    /// @param _gov The new governance address
     function setGov(address _gov) external onlyGov {
         gov = _gov;
     }
 }
+
+// EOF
